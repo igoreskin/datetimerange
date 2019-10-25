af6a63e99d91ca9d31b881d7869b0ce610161ad7
Igor Eskin <eskinhd@gmail.com>
2019-10-25 19:52:26 -0400

    Initial commit

---
+++ src/MainContaner.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+// import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
+import DatetimeRangePicker from 'react-datetime-range-picker';
+import Moment from 'react-moment';
+import 'moment-timezone';
+
+const MainContaner = () => {
+
+    // const [date, setDate] = useState([new Date(), new Date()]);
+
+    const handleChange =()
+
+    return (
+        // <div>
+        //     <DateTimeRangePicker
+        //     onChange={this.onChange}
+        //     value={this.state.date}
+        //     />
+        // </div>
+        <div>
+            <DatetimeRangePicker
+            // onChange={this.onChange}
+            // value={this.state.date}
+            />
+        </div>
+    )
+}
+
+export default MainContaner

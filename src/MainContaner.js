43a84009f15da12fff79f5f290042d1a1215a0fb
Igor Eskin <eskinhd@gmail.com>
2019-10-27 17:28:08 -0400

    Selected dates are displayed in both plain and Unix format

--- src/MainContaner.js
+++ src/MainContaner.js
@@ -1,29 +1,56 @@
-import React, { useState } from 'react';
-// import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
+import React, { useState, Fragment } from 'react';
 import DatetimeRangePicker from 'react-datetime-range-picker';
 import Moment from 'react-moment';
+import moment from 'moment';
 import 'moment-timezone';
 
 const MainContaner = () => {
 
-    // const [date, setDate] = useState([new Date(), new Date()]);
+    const [date, setDate] = useState([]);
+    const startDate = date[0];
+    const endDate= date[1];
 
-    const handleChange =()
+    const handleChange = e => {
+        setDate([e.start, e.end]);
+    } 
+
+    const handleEndDateChange = () => {
+        setApplied(true);
+    }
+
+    const [applied, setApplied] = useState(false);
+    
+    const startInPlainFormat = date[0];
+    const startFormattable = moment(startInPlainFormat).format('YYYY-MM-DD HH:MM:SS');
+    const startInUnixTimestamp = new Date(startFormattable).getTime()/1000;
+
+    const endInPlainFormat = date[1];
+    const endFormattable = moment(endInPlainFormat).format('YYYY-MM-DD HH:MM:SS');
+    const endInUnixTimestamp = new Date(endFormattable).getTime()/1000;
 
     return (
-        // <div>
-        //     <DateTimeRangePicker
-        //     onChange={this.onChange}
-        //     value={this.state.date}
-        //     />
-        // </div>
-        <div>
-            <DatetimeRangePicker
-            // onChange={this.onChange}
-            // value={this.state.date}
-            />
-        </div>
+        <Fragment>
+            <div className="main">
+                <DatetimeRangePicker className="picker" closeOnSelect={true} onChange={e => handleChange(e)} onEndDateChange={handleEndDateChange} />
+            </div>
+            <br />
+            {applied && 
+                <div>
+                    Start Date in plain format:
+                    <div><Moment>{startDate}</Moment></div>
+                    <br />
+                    Start Date - Unix timestamp:
+                    <div>{startInUnixTimestamp}</div>
+                    <br />
+                    End Date in plain format:
+                    <div><Moment>{endDate}</Moment></div>
+                    <br />
+                    End Date - Unix timestamp:
+                    <div>{endInUnixTimestamp}</div>
+                </div>
+            }
+        </Fragment>
     )
 }
 
-export default MainContaner
+export default MainContaner;
